d4cb5fea8e16787465935ba354a39e4074312057
Vineeth <67091853+vineethkvd@users.noreply.github.com>
2023-09-26 14:40:35 +0530

    ok

--- lib/view/screens/splashscreen.dart
+++ lib/view/screens/splashscreen.dart
@@ -15,13 +15,40 @@ class _SplashScreenState extends State<SplashScreen> {
         width: double.infinity,
         height: double.infinity,
         color: const Color(0xFF5529A4),
-        child: Column(
+        child: const Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Row(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Image(image: AssetImage('assets/images/logo.png')),
+                Padding(
+                  padding: EdgeInsets.only(left: 8),
+                  child: Text(
+                    "OFFER",
+                    style: TextStyle(
+                      fontFamily: 'Oswald-Bold',
+                      fontSize: 40,
+                      color: Color(0xFFFFFFFF),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(left: 6),
+                  child: Text(
+                    "NINJA",
+                    style: TextStyle(
+                      fontFamily: 'Oswald-Bold',
+                      fontSize: 40,
+                      color: Color(0xFFF8940E),
+                    ),
+                  ),
+                )
               ],
-            )
+            ),
+            const CircularProgressIndicator(
+              valueColor:  AlwaysStoppedAnimation<Color>(Color(0xFFF8940E)),
+            ),
           ],
         ),
       ),
