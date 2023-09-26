44aa434f480802523dc0a9082f93d5e3e57998fd
Vineeth <67091853+vineethkvd@users.noreply.github.com>
2023-09-26 15:53:44 +0530

    ok

--- lib/view/screens/splashscreen.dart
+++ lib/view/screens/splashscreen.dart
@@ -11,46 +11,53 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        width: double.infinity,
-        height: double.infinity,
-        color: const Color(0xFF5529A4),
-        child: const Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Row(
+      body: Stack(
+        children: [
+          Container(
+            width: double.infinity,
+            height: double.infinity,
+            color: const Color(0xFF5529A4),
+            child: const Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Image(image: AssetImage('assets/images/logo.png')),
-                Padding(
-                  padding: EdgeInsets.only(left: 8),
-                  child: Text(
-                    "OFFER",
-                    style: TextStyle(
-                      fontFamily: 'Oswald-Bold',
-                      fontSize: 40,
-                      color: Color(0xFFFFFFFF),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image(image: AssetImage('assets/images/logo.png')),
+                    Padding(
+                      padding: EdgeInsets.only(left: 8),
+                      child: Text(
+                        "OFFER",
+                        style: TextStyle(
+                          fontFamily: 'Oswald-Bold',
+                          fontSize: 40,
+                          color: Color(0xFFFFFFFF),
+                        ),
+                      ),
                     ),
-                  ),
+                    Padding(
+                      padding: EdgeInsets.only(left: 6),
+                      child: Text(
+                        "NINJA",
+                        style: TextStyle(
+                          fontFamily: 'Oswald-Bold',
+                          fontSize: 40,
+                          color: Color(0xFFF8940E),
+                        ),
+                      ),
+                    )
+                  ],
                 ),
-                Padding(
-                  padding: EdgeInsets.only(left: 6),
-                  child: Text(
-                    "NINJA",
-                    style: TextStyle(
-                      fontFamily: 'Oswald-Bold',
-                      fontSize: 40,
-                      color: Color(0xFFF8940E),
-                    ),
+                Positioned(
+                  child: const CircularProgressIndicator(
+                    valueColor:
+                        AlwaysStoppedAnimation<Color>(Color(0xFFF8940E)),
                   ),
                 )
               ],
             ),
-            const CircularProgressIndicator(
-              valueColor:  AlwaysStoppedAnimation<Color>(Color(0xFFF8940E)),
-            ),
-          ],
-        ),
+          )
+        ],
       ),
     );
   }
