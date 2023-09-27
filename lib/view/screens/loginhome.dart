8b1a68bb5300875b83ec82f8d0e8928c7c38a867
Vineeth <67091853+vineethkvd@users.noreply.github.com>
2023-09-27 14:03:05 +0530

    ok

--- lib/view/screens/loginhome.dart
+++ lib/view/screens/loginhome.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class LoginHome extends StatefulWidget {
-  const LoginHome({super.key});
+  const LoginHome({Key? key});
 
   @override
   State<LoginHome> createState() => _LoginHomeState();
@@ -11,48 +11,101 @@ class _LoginHomeState extends State<LoginHome> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-            leadingWidth: 130,
-            backgroundColor: Colors.white,
-            elevation: 0,
-            automaticallyImplyLeading: false,
-            leading: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Image(
-                  width: 8, image: AssetImage('assets/images/applogo.png')),
-            )),
-        body: Stack(
-          fit: StackFit.expand,
-          children: [
-            Container(
-              color: Colors.white,
-            ),
-            Column(
-              children: [
-                Expanded(
-                    flex: 2,
-                    child: Column(
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.only(top: 90),
-                          child: Image(
-                              image: AssetImage('assets/images/toppng 1.png')),
-                        )
-                      ],
-                    )),
-                Expanded(
-                    flex: 1,
-                    child: Column(
-                      children: [
-                        Text(
-                          "Catch all deal is easier with one tap",
-                          style: TextStyle(color: Color(0xFF4A4A5F)),
-                        )
-                      ],
-                    ))
-              ],
-            )
-          ],
-        ));
+      appBar: AppBar(
+        leadingWidth: 130,
+        backgroundColor: Colors.white,
+        elevation: 0,
+        automaticallyImplyLeading: false,
+        leading: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Image(
+            width: 80,
+            image: AssetImage('assets/images/applogo.png'),
+          ),
+        ),
+      ),
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          Container(
+            color: Colors.white,
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start, // Align children to the start
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Expanded(
+                flex: 2,
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(top: 90),
+                      child: Center(
+                        child: Image(
+                          image: AssetImage('assets/images/toppng 1.png'),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Expanded(
+                flex: 1,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start, // Align text to the start
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(left: 59),
+                      child: Text(
+                        "Catch all deal is \neasier with one tap",
+                        style: TextStyle(
+                          fontFamily: 'Oswald-Bold',
+                          color: Color(0xFF4A4A5F),
+                          fontSize: 30,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 30),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          SizedBox(
+                            width: 133,
+                            height: 46,
+                            child: ElevatedButton(
+                              style: ButtonStyle(
+                                backgroundColor:
+                                MaterialStateProperty.all(Color(0xFF5529A4)),
+                              ),
+                              onPressed: () {},
+                              child: Text("Sign In",style: TextStyle(fontFamily: 'Oswald-Bold'),),
+                            ),
+                          ),
+                          SizedBox(width: 16),
+                          SizedBox(
+                            width: 133,
+                            height: 46,
+                            child: ElevatedButton(
+                              style: ButtonStyle(
+                                backgroundColor:
+                                MaterialStateProperty.all(Color(0xFF5529A4)),
+                              ),
+                              onPressed: () {},
+                              child: Text("Register",style: TextStyle(fontFamily: 'Oswald-Bold'),),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
