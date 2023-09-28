c2e97279a2cb9063450e7a1062ce1fd5278ec359
Vineeth <67091853+vineethkvd@users.noreply.github.com>
2023-09-28 12:18:21 +0530

    ok

--- lib/view/screens/settingspage.dart
+++ lib/view/screens/settingspage.dart
@@ -1,93 +1,117 @@
 import 'package:flutter/material.dart';
 
 class SettingsPage extends StatefulWidget {
-  const SettingsPage({super.key});
+  const SettingsPage({Key? key});
 
   @override
   State<SettingsPage> createState() => _SettingsPageState();
 }
 
 class _SettingsPageState extends State<SettingsPage> {
+  bool isSwitchedNotification = false;
+  bool isSwitchedDarklight = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: Text('Settings'),
+      ),
       body: Padding(
         padding: const EdgeInsets.all(13.0),
-        child: Column(children: [
-          SizedBox(
-            child: Card(
-              child: const ListTile(
+        child: Column(
+          children: [
+            Card(
+              child: ListTile(
                 leading: Icon(Icons.account_circle, size: 36),
                 title: Text('Account'),
               ),
             ),
-          ),
-          SizedBox(
-            child: Card(
-              child: const ListTile(
+            Card(
+              child: ListTile(
                 leading: Icon(Icons.privacy_tip_outlined, size: 36),
                 title: Text('Sonu Nigam'),
               ),
             ),
-          ),
-          SizedBox(
-            child: Card(
-              child: const ListTile(
+            Card(
+              child: ListTile(
                 leading: Icon(Icons.notifications, size: 36),
                 title: Text('Notification'),
+                trailing: Transform.scale(
+                  scale: 0.9, // Adjust the scale factor to change the switch size
+                  child: Switch(
+                    onChanged: (value) {
+                      setState(() {
+                        isSwitchedNotification = value;
+                      });
+                      print('Switch Button is ${isSwitchedNotification ? 'ON' : 'OFF'}');
+                    },
+                    value: isSwitchedNotification,
+                    activeColor: Colors.blue,
+                    activeTrackColor: Colors.yellow,
+                    inactiveThumbColor: Colors.redAccent,
+                    inactiveTrackColor: Colors.orange,
+                  ),
+                ),
               ),
             ),
-          ),
-          SizedBox(
-            child: Card(
-              child: const ListTile(
+            Card(
+              child: ListTile(
                 leading: Icon(Icons.dark_mode, size: 36),
-                title: Text('Darl/light'),
+                title: Text('Dark/Light Mode'),
+                trailing: Transform.scale(
+                  scale: 0.9, // Adjust the scale factor to change the switch size
+                  child: Switch(
+                    onChanged: (value) {
+                      setState(() {
+                        isSwitchedDarklight = value;
+                      });
+                      print('Switch Button is ${isSwitchedDarklight ? 'ON' : 'OFF'}');
+                    },
+                    value: isSwitchedDarklight,
+                    activeColor: Colors.blue,
+                    activeTrackColor: Colors.yellow,
+                    inactiveThumbColor: Colors.redAccent,
+                    inactiveTrackColor: Colors.orange,
+                  ),
+                ),
               ),
             ),
-          ),
-          SizedBox(
-            child: Card(
-              child: const ListTile(
+            Card(
+              child: ListTile(
                 leading: Icon(Icons.language, size: 36),
                 title: Text('Language'),
               ),
             ),
-          ),
-          SizedBox(
-            child: Card(
-              child: const ListTile(
+            Card(
+              child: ListTile(
                 leading: Icon(Icons.album, size: 36),
                 title: Text('Sonu Nigam'),
               ),
             ),
-          ),
-          SizedBox(
-            child: Card(
-              child: const ListTile(
+            Card(
+              child: ListTile(
                 leading: Icon(Icons.password, size: 34),
-                title: Text('Change password'),
+                title: Text('Change Password'),
               ),
             ),
-          ),
-          SizedBox(
-            child: Card(
-              child: const ListTile(
+            Card(
+              child: ListTile(
                 leading: Icon(Icons.delete, size: 36),
-                title: Text('Delete account'),
+                title: Text('Delete Account'),
               ),
             ),
-          ),
-          SizedBox(
-            child: Card(
-              child: const ListTile(
-                leading: Icon(Icons.logout,size: 36,),
-                title: Text('Log out'),
+            Card(
+              child: ListTile(
+                leading: Icon(
+                  Icons.logout,
+                  size: 36,
+                ),
+                title: Text('Log Out'),
               ),
             ),
-          ),
-        ]),
+          ],
+        ),
       ),
     );
   }
