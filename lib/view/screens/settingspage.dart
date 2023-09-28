2607a423191afdc7a81f6f8998ffecb94e96f63f
Vineeth <67091853+vineethkvd@users.noreply.github.com>
2023-09-28 11:51:45 +0530

    ok

--- lib/view/screens/settingspage.dart
+++ lib/view/screens/settingspage.dart
@@ -12,73 +12,83 @@ class _SettingsPageState extends State<SettingsPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Column(children: [
-        SizedBox(
-          child: Card(
-            child: const ListTile(
-              leading: Icon(Icons.account_circle, size: 36),
-              title: Text('Account'),
+      body: Padding(
+        padding: const EdgeInsets.all(13.0),
+        child: Column(children: [
+          SizedBox(
+            child: Card(
+              child: const ListTile(
+                leading: Icon(Icons.account_circle, size: 36),
+                title: Text('Account'),
+              ),
             ),
           ),
-        ),
-        SizedBox(
-          child: Card(
-            child: const ListTile(
-              leading: Icon(Icons.privacy_tip_outlined, size: 36),
-              title: Text('Sonu Nigam'),
+          SizedBox(
+            child: Card(
+              child: const ListTile(
+                leading: Icon(Icons.privacy_tip_outlined, size: 36),
+                title: Text('Sonu Nigam'),
+              ),
             ),
           ),
-        ),
-        SizedBox(
-          child: Card(
-            child: const ListTile(
-              leading: Icon(Icons.notifications, size: 36),
-              title: Text('Notification'),
-              subtitle: Text('Best of Sonu Nigam Song'),
+          SizedBox(
+            child: Card(
+              child: const ListTile(
+                leading: Icon(Icons.notifications, size: 36),
+                title: Text('Notification'),
+              ),
             ),
           ),
-        ),
-        SizedBox(
-          child: Card(
-            child: const ListTile(
-              leading: Icon(Icons.dark_mode, size: 36),
-              title: Text('Darl/light'),
+          SizedBox(
+            child: Card(
+              child: const ListTile(
+                leading: Icon(Icons.dark_mode, size: 36),
+                title: Text('Darl/light'),
+              ),
             ),
           ),
-        ),
-        SizedBox(
-          child: Card(
-            child: const ListTile(
-              leading: Icon(Icons.language, size: 36),
-              title: Text('Language'),
+          SizedBox(
+            child: Card(
+              child: const ListTile(
+                leading: Icon(Icons.language, size: 36),
+                title: Text('Language'),
+              ),
             ),
           ),
-        ),
-        SizedBox(
-          child: Card(
-            child: const ListTile(
-              leading: Icon(Icons.album, size: 36),
-              title: Text('Sonu Nigam'),
+          SizedBox(
+            child: Card(
+              child: const ListTile(
+                leading: Icon(Icons.album, size: 36),
+                title: Text('Sonu Nigam'),
+              ),
             ),
           ),
-        ),
-        SizedBox(
-          child: Card(
-            child: const ListTile(
-              leading: Icon(Icons.password, size: 34),
-              title: Text('Change password'),
+          SizedBox(
+            child: Card(
+              child: const ListTile(
+                leading: Icon(Icons.password, size: 34),
+                title: Text('Change password'),
+              ),
             ),
           ),
-        ),
-        SizedBox(
-          child: Card(
-            child: const ListTile(
-              leading: Icon(Icons.delete, size: 36),
-              title: Text('Delete account'),
+          SizedBox(
+            child: Card(
+              child: const ListTile(
+                leading: Icon(Icons.delete, size: 36),
+                title: Text('Delete account'),
+              ),
             ),
           ),
-        ),
-      ]),
+          SizedBox(
+            child: Card(
+              child: const ListTile(
+                leading: Icon(Icons.logout,size: 36,),
+                title: Text('Log out'),
+              ),
+            ),
+          ),
+        ]),
+      ),
     );
   }
 }
